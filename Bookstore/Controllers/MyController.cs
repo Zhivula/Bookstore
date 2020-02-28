d7539aedfc4aa56c6747b68414066b350396685c
Zhivula <56290740+Zhivula@users.noreply.github.com>
2020-02-28 09:47:43 +0300

    Добавлен собственный контроллер MyController. Мастер-страница теперь подключается к представлениям.

---
+++ Bookstore/Controllers/MyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Bookstore.Controllers
+{
+    public class MyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string E(int i=10)
+        {
+            return "This is your IP";
+        }
+
+        [HttpGet]
+        public ActionResult E()
+        {
+            return View();
+        }
+    }
+}
