9124ef0129287d12270d54b3c999d2c4987f6f2c
Zhivula <56290740+Zhivula@users.noreply.github.com>
2020-03-16 10:49:47 +0300

    Добавлены собственные ActionResult

--- Bookstore/Controllers/MyController.cs
+++ Bookstore/Controllers/MyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bookstore.Util;
 using System.Web.Routing;
 
 namespace Bookstore.Controllers
@@ -16,13 +17,22 @@ namespace Bookstore.Controllers
         [HttpPost]
         public string E(int i=10)
         {
-            return "This is your IP";
+            return "This is your number"+i;
         }
 
         [HttpGet]
         public ActionResult E()
         {
             return View();
+        }
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Hello, world. This is my page)))</h2>");
+        }
+        public ActionResult GetImage()
+        {
+            string path = "../Image/imgh.jpg";
+            return new ImageResult(path);
         }
     }
 }
